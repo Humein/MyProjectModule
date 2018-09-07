94b22a7f52e62c89a8801913e5336de124be35e0
Zhang Xin Xin <2206988863@qq.com>
2018-09-07 20:18:13 +0800

    www

---
+++ MyProjectModule/MyProjectModule/BezierPath&ShapLayer&Animation/DrawWaveLine/StartWaterView.h
@@ -0,0 +1,15 @@
+//
+//  StartWaterView.h
+//  MyProjectModule
+//
+//  Created by Zhang Xin Xin on 2018/9/7.
+//  Copyright © 2018年 xinxin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface StartWaterView : UIView
+@property (nonatomic, strong) UIImage *maskImage;
+@property (nonatomic, strong) UIImage *borderImage;
+@property (nonatomic, strong) UIColor *fillColor;
+@end
