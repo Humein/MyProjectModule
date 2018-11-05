0480407530c8dbec7f4fda50805a44b4970e9342
Zhang Xin Xin <shou1wang@gmail.com>
2018-11-05 11:46:58 +0800

    方案

--- MyProjectModule/MyProjectModule/DesignMode/责任链模式/对外API/旋转屏解决/FullScreenViewController.h
+++ MyProjectModule/MyProjectModule/DesignMode/责任链模式/对外API/旋转屏解决/FullScreenViewController.h
@@ -9,8 +9,9 @@
 #import <UIKit/UIKit.h>
 
 /*
- 在一个只支持Portrait的ViewController上，present一个只支持Landscape的ViewController，通过改写ViewController之间的转场动画，既能高度自定义全屏动画，也能让StatusBar在视频全屏时横向显示。
-还有一个
+ 1 在一个只支持Portrait的ViewController上，present一个只支持Landscape的ViewController，通过改写ViewController之间的转场动画，既能高度自定义全屏动画，也能让StatusBar在视频全屏时横向显示。
+ 
+ 2 还有一个
  
  */
 
--- MyProjectModule/MyProjectModule/TableViewModule/折叠的TableView/ThreeLevelTreeView.h
+++ MyProjectModule/MyProjectModule/TableViewModule/折叠的TableView/ThreeLevelTreeView.h
@@ -6,6 +6,18 @@
 //  Copyright © 2018年 xinxin. All rights reserved.
 //
 
+
+/*
+   场景1
+ 将 TreeView 当作当前页面的骨架，其他元素 放在 Header 和 Footer 里
+ 
+   场景2
+ 将 TreeView 当作当前页面的某个类型 Cell， 这个组合星比较高 适应场景丰富
+ 
+ 
+*/
+
+
 #import <UIKit/UIKit.h>
 
 @interface ThreeLevelTreeView : UIView
