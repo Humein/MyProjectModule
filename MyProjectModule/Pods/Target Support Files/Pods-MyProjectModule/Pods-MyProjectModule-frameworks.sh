3cd2f000538644af66c6b41a385fc2898ebbe729
Zhang Xin Xin <shou1wang@gmail.com>
2019-03-11 20:33:34 +0800

    3213

--- MyProjectModule/Pods/Target Support Files/Pods-MyProjectModule/Pods-MyProjectModule-frameworks.sh
+++ MyProjectModule/Pods/Target Support Files/Pods-MyProjectModule/Pods-MyProjectModule-frameworks.sh
@@ -162,7 +162,7 @@ strip_invalid_archs() {
 
 if [[ "$CONFIGURATION" == "Debug" ]]; then
   install_framework "${BUILT_PRODUCTS_DIR}/AFNetworking/AFNetworking.framework"
-  install_framework "${PODS_ROOT}/../../../../AndroidStudioProjects/flutter_module/.ios/Flutter/engine/Flutter.framework"
+  install_framework "${PODS_ROOT}/../../../AndroidStudioProjects/flutter_module/.ios/Flutter/engine/Flutter.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/FlutterPluginRegistrant/FlutterPluginRegistrant.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/KVOController/KVOController.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/MJExtension/MJExtension.framework"
@@ -175,7 +175,7 @@ if [[ "$CONFIGURATION" == "Debug" ]]; then
 fi
 if [[ "$CONFIGURATION" == "Release" ]]; then
   install_framework "${BUILT_PRODUCTS_DIR}/AFNetworking/AFNetworking.framework"
-  install_framework "${PODS_ROOT}/../../../../AndroidStudioProjects/flutter_module/.ios/Flutter/engine/Flutter.framework"
+  install_framework "${PODS_ROOT}/../../../AndroidStudioProjects/flutter_module/.ios/Flutter/engine/Flutter.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/FlutterPluginRegistrant/FlutterPluginRegistrant.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/KVOController/KVOController.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/MJExtension/MJExtension.framework"
