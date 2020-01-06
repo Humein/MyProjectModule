a92317b49c3c582ade5fc3e25cd209b0552f6951
Zhang Xin Xin <shou1wang@gmail.com>
2020-01-06 09:50:32 +0800

    update

--- MyProjectModule/MyProjectModule/DesignMode/Protocol Oriented Programming/Design-Pattern-in-Swift/Protocol Oriented Programming/ShakeView.swift
+++ MyProjectModule/MyProjectModule/DesignMode/Protocol Oriented Programming/Design-Pattern-in-Swift/Protocol Oriented Programming/ShakeView.swift
@@ -4,10 +4,32 @@
 //
 //  Created by Zhang Xin Xin on 2019/12/3.
 //  Copyright © 2019 xinxin. All rights reserved.
-/*
-   Swift的 POP 是使用了继承的思想，它模拟了多继承关系，实现了代码的跨父类复用，同时也不存在 is-a 关系。swift中主类和 extension扩展类 的协同工作，保留了 在主类中定义方法 在所继承的类中进行实现的特性，又新增了在 extension拓展类 中定义并实现的方法在任何继承自此协议的类中可以任意调用，从而实现组件化编程。
-*/
+
 import UIKit
+
+/**
+  - 对于以下重复功能的处理
+    - 基类
+      -  抽象出一个基类 FoodImageView ActionButton,ActionObject 继承它
+          缺点：
+           - 可读性很差，例如，对于 foodImageView/actionButton/ActionObject 来说，你看不出来任何抖动的意图。整个类里面没有任何东西能告诉你它需要抖动。这样表意不明确，因为别处可能会随机存在一个抖动函数，你甚至不知道它是从哪里来的。
+           - 随着业务增加，基类会越来越臃肿。
+    - 分类
+      - 对他们的基类UIView进行扩展
+        缺点:
+        对于ActionObject就无法适用了
+    - 协议
+      - 功能具有插拔性
+      - 适用场景广泛
+      - 避免一个类其实只要一个功能情况下，也要继承着多个功能模块
+ 
+  - 个人理解：
+        - 利用Swift协议默认实现特性中代替继承/分类添加功能，避免继承的臃肿和分类的移植性差
+        使用协议代替继承 可以更好的实现功能(组建)的 插拔性。    避免一个类其实只要一个功能情况下，也要继承着多个功能模块，同时基类中功能会慢慢越来越臃肿。 而且还可以将功能抽象的添加到不同类中(View和VC 继承同一种协议)
+        基类的作用是统一管理，统一风格，便于编码，有更多的额外的附加功能的话，建议使用Protocol 或 Category，这样移植性强，便于管理与扩展，不至于牵一发而动全身。
+      
+ */
+
 
 class FoodImageView: UIImageView, ShakeDelegate, CloseDelegate{
 
@@ -30,31 +52,31 @@ class ActionObject: NSObject, ShakeDelegate, CloseDelegate{
     }
 }
 
-/*
-      以上优化有两种方案
-      1: 使用继承(抽象出一个View ActionButton,FoodImageView 都会继承它) /  分类(UIView)
-         缺点：
-             可读性很差了。例如，对于 foodImageView 和 actionButton 来说，你看不出来任何抖动的意图。整个类里面没有任何东西能告诉你它需要抖动。这样表意不明确，因为别处可能会随机存在一个抖动函数，你甚至不知道它是从哪里来的。
-      2: 使用协议
- 
- 
-个人理解：
- 利用Swift协议默认实现特性中代替继承/分类添加功能，避免继承的臃肿和分类的移植性差
-       使用协议代替继承 可以更好的实现功能(组建)的 插拔性。 避免一个类其实只要一个功能情况下，也要继承着多个功能模块，同时基类中功能会慢慢越来越臃肿。 而且还可以将功能抽象的添加到不同类中(View和VC 继承同一种协议)
-       基类的作用是统一管理，统一风格，便于编码，有更多的额外的附加功能的话，建议使用Protocol 或 Category，这样移植性强，便于管理与扩展，不至于牵一发而动全身。
- */
 
-//MARK: - 1: 使用继承
+
+//MARK: - 1: 基类
+class BaseObject{
+    func base_shake() {
+        print("base_shake")
+    }
+}
+
+//MARK: - 2: 扩展
 extension UIView {
     func ex_shake() {
         print("ex_shake")
     }
 }
 
+extension ActionObject {
+    func ex_shake() {
+        print("ex_shake")
+    }
+}
 
-//MARK: - 2: 使用协议
+//MARK: - 3: 协议
 protocol ShakeDelegate { }
-// 默认实现 where Self: UIView 限制调用类型
+// 默认实现 where  限制类型
 extension ShakeDelegate where Self: UIView {
     func protocol_shake() {
         print("protocol_shake")
@@ -70,7 +92,7 @@ extension CloseDelegate {
 }
 
 
-//MARK: - 3: 使用协议 代替工厂模式
+//MARK: - 4: 协议模拟工厂模式 参考 FactoryObject
 protocol InitObjectProtocol {
     func initObject() -> Any
     var abutton : UIButton? { get set }
@@ -97,5 +119,13 @@ class UIViewA: UIView, InitObjectProtocol{
 
 class ProtocolHeler: NSObject {
     var dele: InitObjectProtocol? = nil
-
+    
+    init(_ model: Any) {
+        let view = dele?.initObject()
+        let button = dele?.initObject()
+        
+        print(view.debugDescription)
+        print(button.debugDescription)
+    }
+    
 }
