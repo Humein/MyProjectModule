086e2e687b5ad898e128fa3620f1ce669aa30547
Zhang Xin Xin <shou1wang@gmail.com>
2020-05-12 22:52:23 +0800

    进度

--- MyProjectModule/MyProjectModule/DesignMode/Protocol Oriented Programming/Design-Pattern-in-Swift/SubTwoSwiftDemosViewController.swift
+++ MyProjectModule/MyProjectModule/DesignMode/Protocol Oriented Programming/Design-Pattern-in-Swift/SubTwoSwiftDemosViewController.swift
@@ -28,22 +28,137 @@ class SubTwoSwiftDemosViewController: SwiftDemosViewController {
     }
 }
 
-//
-class GradientProgressView: UIProgressView {
+// 播放进度
+protocol SDPlaySliderDelegate {
+    func sliderValueChanging(_ slider: SDGradientSlider)
+}
+class SDGradientSlider: UIView{
+    var touchView = UIView()
+    var thumbImageView = UIImageView()
+    var foregroundView = UIView()
+    var kThumbW: CGFloat?
+    var kSliderW: CGFloat?
+    var kSliderH: CGFloat?
+    var value: CGFloat?
+    var delegate: SDPlaySliderDelegate?
+
+
+    lazy var gradientLayer:CAGradientLayer = {
+        let gradientLayer = CAGradientLayer()
+        gradientLayer.cornerRadius = 10
+        gradientLayer.colors = [UIColor.yellow.cgColor,UIColor.red.cgColor]
+        gradientLayer.startPoint = CGPoint.init(x: 0, y: 0)
+        gradientLayer.endPoint = CGPoint.init(x: 1, y: 0)
+        gradientLayer.locations = [0,1]
+        return gradientLayer
+        
+    }()
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        initWidget()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    func initWidget(){
+        self.isUserInteractionEnabled = true
+        kSliderW = self.bounds.size.width
+        kSliderH = self.bounds.size.height
+        kThumbW = 20
+        value = 0
+        thumbImageView.isUserInteractionEnabled = true
+        self.addSubview(foregroundView)
+        self.addSubview(thumbImageView)
+        self.layer.masksToBounds = true
+        self.layer.cornerRadius = 10
+        self.backgroundColor = .black
+        thumbImageView.backgroundColor = .blue
+        touchView = thumbImageView
+        let point = CGPoint.init(x: 0, y: 0)
+        fillForeGroundViewWithPoint(point)
+    }
+    
+    func setSliderValue(_ value: CGFloat){
+        var v = value
+        if (v > 1) {
+            v = 1
+        }
+        let point = CGPoint.init(x: v * kSliderW!, y: 0)
+        fillForeGroundViewWithPoint(point)
+    }
+    
+    
+    func fillForeGroundViewWithPoint(_ point: CGPoint){
+        var p = point
+        p.x += CGFloat(kThumbW!/2)
+        if (p.x > kSliderW!) {
+            p.x = kSliderW!
+        }
+        if (p.x < 0) {
+            p.x = 0
+        }
+        value = p.x / kSliderW!
+        if value == 1.0 || value == 0.0 {
+            return
+        }
+        foregroundView.frame = CGRect.init(x: 0, y: 0, width: point.x, height: kSliderH!)
+        if (foregroundView.frame.size.width <= 0) {
+            thumbImageView.frame = CGRect.init(x: 0, y: 0, width: kThumbW!, height: kSliderH!)
+        }else if (foregroundView.frame.size.width >= kSliderW!) {
+            thumbImageView.frame = CGRect.init(x: foregroundView.frame.size.width - kThumbW!, y: 0, width: kThumbW!, height: kSliderH!)
+        }else{
+            thumbImageView.frame = CGRect.init(x: foregroundView.frame.size.width - kThumbW!, y: 0, width: kThumbW!, height: kSliderH!)
+        }
+        gradientLayer.frame = foregroundView.bounds
+        gradientLayer.cornerRadius = 10
+        foregroundView.layer.addSublayer(gradientLayer)
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        let touch = ((touches as NSSet).anyObject() as AnyObject)
+        if touchView == thumbImageView {
+            return
+        }
+        let point = touch.location(in:self)
+        fillForeGroundViewWithPoint(point)
+    }
+    
+    override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
+        let touch = ((touches as NSSet).anyObject() as AnyObject)
+        if touch.view != touchView {
+            return
+        }
+        let point = touch.location(in:self)
+        fillForeGroundViewWithPoint(point)
+        delegate?.sliderValueChanging(self)
+    }
+    
+    override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
+        let touch = ((touches as NSSet).anyObject() as AnyObject)
+        if touch.view != touchView {
+            return
+        }
+        let point = touch.location(in:self)
+        fillForeGroundViewWithPoint(point)
+        
+    }
     
 }
 
 
+
 // 知识点播放进度
-class SDKnowledgePlayerSliderView: UIView {
+class SDKnowledgePlayerSliderView: UIView, SDPlaySliderDelegate{
+    
     lazy var currentTimeLabel: UILabel = {
         var inputBarrage = UILabel.init(frame: CGRect.init(x: 0, y: 0, width: 40, height: 17))
         inputBarrage.backgroundColor = .clear
         inputBarrage.text = "6:00"
         inputBarrage.font = UIFont.init(name: "PingFangSC-Regular", size: 12)
         inputBarrage.textAlignment = .center
-        inputBarrage.textColor = .white // #FFFFFF
-        inputBarrage.clipsToBounds = true
+        inputBarrage.textColor = .red // #FFFFFF
         return inputBarrage
     }()
     lazy var totalTimeLabel: UILabel = {
@@ -52,11 +167,11 @@ class SDKnowledgePlayerSliderView: UIView {
         inputBarrage.text = "12:00"
         inputBarrage.font = UIFont.init(name: "PingFangSC-Regular", size: 12)
         inputBarrage.textAlignment = .center
-        inputBarrage.textColor = .white // #FFFFFF
+        inputBarrage.textColor = .red // #FFFFFF
         return inputBarrage
     }()
-    lazy var playerSlider: GradientProgressView = {
-        var pointButton = GradientProgressView.init(frame: CGRect.init(x: 0, y: 0, width: 310, height: 24))
+    lazy var playerSlider: SDGradientSlider = {
+        var pointButton = SDGradientSlider.init(frame: CGRect.init(x: 0, y: 0, width: self.frame.size.width - 10 - 80, height: 24))
         return pointButton
     }()
     
@@ -70,8 +185,30 @@ class SDKnowledgePlayerSliderView: UIView {
     }
     
     func initWidget(){
+        playerSlider.delegate = self
+        self.addSubview(currentTimeLabel)
+        self.addSubview(totalTimeLabel)
         self.addSubview(playerSlider)
+        currentTimeLabel.snp.makeConstraints { (make) in
+            make.centerY.equalTo(self)
+            make.left.equalTo(self).offset(10)
+        }
+        totalTimeLabel.snp.makeConstraints { (make) in
+            make.centerY.equalTo(self)
+            make.right.equalTo(self).offset(-10)
+        }
+        playerSlider.snp.makeConstraints { (make) in
+            make.centerY.equalTo(self)
+            make.left.equalTo(currentTimeLabel.snp_right).offset(10)
+            make.size.equalTo(CGSize.init(width: self.frame.size.width - 10 - 80, height: 24))
+        }
     }
+    
+    func sliderValueChanging(_ slider: SDGradientSlider) {
+        print(slider.value as Any)
+    }
+    
+    
 }
 
 // 知识点播放bar
@@ -95,8 +232,8 @@ class SDKnowledgePlayerBottomView: UIView {
         return pointButton
     }()
     lazy var playerSliderView: SDKnowledgePlayerSliderView = {
-        var pointButton = SDKnowledgePlayerSliderView.init(frame: CGRect.init(x: 0, y: 0, width: 0, height: 0))
-        pointButton.backgroundColor = .clear
+        var pointButton = SDKnowledgePlayerSliderView.init(frame: CGRect.init(x: 0, y: 0, width: self.frame.size.width - (rightPointButton.viewsWidth * 2), height: 24))
+        pointButton.backgroundColor = .white
         return pointButton
     }()
     
@@ -116,7 +253,7 @@ class SDKnowledgePlayerBottomView: UIView {
         self.addSubview(leftPointButton)
         self.addSubview(rightPointButton)
         self.addSubview(playerSliderView)
-
+        playerSliderView.playerSlider.setSliderValue(0.5)
 
         leftPointButton.snp.makeConstraints { (make) in
             make.centerY.equalTo(self)
@@ -130,9 +267,9 @@ class SDKnowledgePlayerBottomView: UIView {
         }
         playerSliderView.snp.makeConstraints { (make) in
             make.centerY.equalTo(self)
-            make.right.equalTo(rightPointButton.snp_left).offset(0)
-            make.left.equalTo(leftPointButton.snp_right).offset(0)
-        }        
+            make.centerX.equalTo(self)
+            make.size.equalTo(CGSize.init(width: self.frame.size.width - (rightPointButton.viewsWidth * 2), height: 24))
+        }
         
     }
 }
