085ef0af7f80ffe04a64bf85a3a0fd1a7b290f5c
Zhang Xin Xin <shou1wang@gmail.com>
2021-02-20 16:58:32 +0800

    1

--- MyProjectModule/MyProjectModule/TableViewModule/ReadMe.h
+++ MyProjectModule/MyProjectModule/TableViewModule/ReadMe.h
@@ -8,6 +8,26 @@
 
 //*********开发小结***********//  实战在项目中，理论放在markdown中。
 
+
+0.  自动布局拾遗，避免遗忘。。。
+
+  0.1： 顶部控件如果没有，底部控件上移。如果顶部控件高度没有设置，底部控件会自动上移。否则，需要你手动更新顶部控件高度为0 ！！！
+
+  0.2：外部修改内部控件的布局，可以在setModel 中更新。
+
+  0.3：修改UILabel内边距
+重写UILabel
+-(CGSize)intrinsicContentSize{
+    CGSize originalSize = [super intrinsicContentSize];
+    CGSize size = CGSizeMake(originalSize.width+8, originalSize.height);
+    return size;
+}
+
+  0.4: CollectionView cell宽度是可以自适应。
+flowLayout.estimatedItemSize = CGSizeMake(74, 44);
+
+
+
 1.  UITableViewCell嵌套 UITableView / CollectionView 时计算高度。通过 contentSize配合layoutIfNeeded 来获取
 
 [self.tableView reloadData];
@@ -16,7 +36,16 @@
     make.height.mas_equalTo(self.tableView.contentSize.height);
 }];
 
-2.
+2.  记录Cell的预估高度
+
+- (void)tableView:(UITableView *)tableView willDisplayCell:(UITableViewCell *)cell forRowAtIndexPath:(NSIndexPath *)indexPath {
+    LevelList *model = [_model.levelList xesApp_objectOrNilAtIndex:indexPath.row];
+    //高度缓存
+    if (model.cell_height == 0) {
+        CGFloat height = cell.height;
+        model.cell_height = height;
+    }
+}
 
 
 
