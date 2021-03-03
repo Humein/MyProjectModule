aa72cf2eb26849f08372e64d03a72e7676b1905d
Zhang Xin Xin <shou1wang@gmail.com>
2021-03-03 14:36:12 +0800

    1

---
+++ MyProjectModule/MyProjectModule/PodProject Analysis/YYKits/YYCategory/CALayer+YYAddXX.h
@@ -0,0 +1,27 @@
+//
+//  CALayer+YYAddXX.h
+//  MyProjectModule
+//
+//  Created by zhangxinxin on 2021/3/3.
+//  Copyright © 2021 xinxin. All rights reserved.
+//
+/*===================================================
+        * 文件描述 ：CALayer+YYAdd 功能list *
+  1. view/layer 截图&转图片
+  2. view/layer 转PDF
+  3. 设置layer的阴影
+  4. Remove all sublayers
+  5. layer frame的各种简写
+  6. 当内容改变时，为图层的内容添加一个渐隐动画。
+  7. 取消使用"-addFadeAnimationWithDuration:curve:"添加的淡出动画。
+=====================================================*/
+#import <QuartzCore/QuartzCore.h>
+#import "CALayer+YYAdd.h"
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface CALayer_YYAddXX : CALayer
+
+@end
+
+NS_ASSUME_NONNULL_END
